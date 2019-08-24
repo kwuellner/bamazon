108d0b953ba57274b1210cb14000d6ccb7d3e02b
Kyle Wuellner <rockstar81@comcast.net>
2019-08-23 23:47:24 -0500

    added some functions and prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,37 @@ let connection = mysql.createConnection({
     database: "bamazon"
 });
 
+
+function bamProducts() {
+
+    connection.connect(function (error) {
+        if (error) throw error;
+
+        console.log(colors.green("---------------\n"));
+        console.log(colors.bold.yellow("Welcome To Bamazon!"));
+        console.log(colors.green("\n---------------\n"));
+        for (var i = 0; i < data.length; i++) {
+            console.log(colors.bold.bgCyan("Item ID: " + data[i].item_id + " | " + "Product Name: " + data[i].product_name + " | " + "Department Name: " + data[i].department_name + " | " + "Price: " + data[i].price + " | " + "Stock Quantity: " + data[i].stock_quantity));
+            console.log(colors.green("\n--------------------------"));
+        }
+        buyProducts();
+    });
+};
+
+function buyProducts() {
+    inquirer.prompt([{
+        type: "number",
+        name: "id",
+        message: "Please enter the product ID you are interested in:"
+    },
+    {
+        type: "number",
+        name: "qty",
+        message: "How many would you like?"
+
+    }]).then(function (answer) {
+        let productId = answer.id;
+        let quantity = answer.qty;
+        totalProduct(productId, quantity)
+    });
+};
